5dabc3a3a3ccff174c88d77ea81deebc652cdb39
elena-zelenskaya <elena.a.zelenskaya@gmail.com>
2021-07-13 17:28:32 -0700

    add admin.py

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+from plant.models import *
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'password', 'profile_picture', 'user_address')
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'line1', 'line2', 'city', 'state', 'zip_code', 'user')
+
